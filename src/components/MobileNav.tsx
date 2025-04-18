84f8af27f8aac463783c3d72ef8f41819e7f65de
Tn <asisduous@gmail.com>
2025-04-18 01:54:47 -0700

    modified mobilenav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,6 +1,5 @@
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from './ui/sheet'
 import { CircleUserRound, Menu } from 'lucide-react'
-import { Separator } from '@radix-ui/react-separator'
 import { Button } from './ui/button'
 import { useAuth0 } from '@auth0/auth0-react'
 import MobileNavLinks from './MobileNavLinks'
